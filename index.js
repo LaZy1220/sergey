563b8af4e594de7fded95981d0c21d2aedd774bf
LaZy1220 <4elove4iska@gmail.com>
2022-09-02 23:42:44 +0300

    SERGEEEEEY!!! PO4TI VSE!!!

--- index.js
+++ index.js
@@ -1,3 +1,120 @@
+window.onload=function(){
+    const navigate = document.querySelector('.navigate-body')
+    const lineParent = document.querySelector('[data-line-parent]')
+    const lineItems = document.querySelectorAll('[data-line-item]')
+    const lineBlocks  =document.querySelectorAll('[data-line-block]')
+    if(lineParent){
+        lineParent.insertAdjacentHTML(
+            'beforeend',
+            '<div class="line"></div>'
+        )
+    }
+    const lineStyle =getComputedStyle(document.querySelector('.line'))
+    const lineTransition = lineStyle.transition
+    function setValues(target, hide){
+        const line = target.closest('[data-line-parent]').querySelector('.line')
+        if(!hide){
+            const width = target.getBoundingClientRect().width
+            const leftItem = target.getBoundingClientRect().left
+            const bottomItem = target.getBoundingClientRect().bottom
+            const leftParent = target.closest('[data-line-parent]').getBoundingClientRect().left
+            const bottomParent = target.closest('[data-line-parent]').getBoundingClientRect().bottom
+            const left = leftItem - leftParent
+            const bottom = bottomParent - bottomItem
+            line.style.width = `${width}px`
+            line.style.bottom = `${bottom}px`
+            line.style.left = `${left}px`
+        }
+        else{
+            line.style.width='0'
+        }
+    }
+    function mouseenterFunc(){
+        setValues(this)
+    }
+    function mouseleaveFunc(){
+        const lineItemActive= document.querySelector("[data-line-item]._active")
+        if(lineItemActive){
+            setValues(lineItemActive)
+        }
+        else{
+            setValues(this,true)
+        }
+    }
+    function resizeFunc(){
+        const lineItemActive = document.querySelector('[data-line-item._active]')
+        if(lineItemActive){
+            const line = lineItemActive.closest('[data-line-parent]').querySelector('.line')
+            line.style.transition='none'
+            setValues(lineItemActive)
+            line.style.transition=lineTransition
+        }
+    }
+    function clickFunc(e){
+        if(e.target.closest('[data-line-item]')){
+            const lineItemValue = e.target.closest('[data-line-item]').getAttribute('data-line-item')
+            if(lineItemValue){
+                const blockScrollTo = document.querySelector(`[data-line-block="${lineItemValue}"]`)
+                const lineItemActive = document.querySelector('[data-line-item]._active')
+                lineBlocks.forEach(lineBlock=>{
+                    observer.unobserve(lineBlock)
+                })
+                if(lineItemActive){
+                    lineItemActive.classList.remove('_active')
+                }
+                e.target.closest('[data-line-item]').classList.add('_active')
+                if(blockScrollTo){
+                    const blockScrollToTop=blockScrollTo.getBoundingClientRect().top+pageYOffset
+                    window.scrollTo({
+                        top: blockScrollToTop,
+                        behavior: "smooth"
+                    })
+                }
+                setTimeout(() => {
+                    lineBlocks.forEach(lineBlock=>{
+                        observer.observe(lineBlock)
+                    })
+                }, 500);
+            }
+        }
+    }
+    document.addEventListener('click',clickFunc);
+    window.addEventListener('resize',resizeFunc);
+    lineItems.forEach(lineItem => {
+        lineItem.addEventListener('mouseenter', mouseenterFunc)
+        lineItem.addEventListener('mouseleave',mouseleaveFunc)
+    })
+    const changeLineItem = (entries, observer)=>{
+        entries.forEach(entry=>{
+            if(entry.isIntersecting){
+                const blockVisible = entry.target.getAttribute('data-line-block')
+                const lineItemVisible = document.querySelector(`[data-line-item="${blockVisible}"]`)
+                if(lineItemVisible){
+                    lineItemVisible.classList.add('_active')
+                    setTimeout(() => {
+                        setValues(lineItemVisible)
+                    }, 1 )
+                }
+            }
+            else{
+                const blockVisible = entry.target.getAttribute('data-line-block')
+                const lineItemVisible = document.querySelector(`[data-line-item="${blockVisible}"]`)
+                if(lineItemVisible){
+                    lineItemVisible.classList.remove('_active')
+                    setValues(lineItemVisible, true)
+                }
+            }
+        })
+    }
+    const options ={
+        threshold:0.4
+    }
+    const observer = new IntersectionObserver(changeLineItem, options)
+    lineBlocks.forEach(lineBlock=>{
+        observer.observe(lineBlock)
+    })
+}
+
 const addOrderButton = document.querySelector('.button-add-service')
 const closeWindow =document.querySelector('.close')
 const addOrderWindow =document.querySelector('.add-order')
@@ -7,6 +124,6 @@ const addOrderWindowHide = ()=>{
 const addOrderWindowShow = ()=>{
     addOrderWindow.style.display='block'
 }
-console.log(addOrderButton,addOrderWindow,closeWindow);
+
 addOrderButton.addEventListener('click',addOrderWindowShow)
 closeWindow.addEventListener('click',addOrderWindowHide)
